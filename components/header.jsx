d26819ab3090dbfdb96389be545be3f1a844f980
Bayekula-hb <hobedbayekula@gmail.com>
2021-08-11 14:16:37 +0100

    refactor add the link of bootsrap in the _app.js

--- components/header.jsx
+++ components/header.jsx
@@ -12,9 +12,9 @@ const Header =()=>{
     return(
         <header>
             <div className="div-container container">
-                <div className="div-date-reseaux">
-                    <div className="div-date">{dateState}</div>
-                    <div className="div-reseaux">face instagr </div>
+                <div className="div-date-reseaux row">
+                    <div className="div-date col-3">{dateState}</div>
+                    <div className="div-reseaux col-4">face instagr </div>
                 </div>
             </div>
         </header>
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import '../styles/globals.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import "../sass/main.scss"
 
 function MyApp({ Component, pageProps }) {
