4616db0bc057dc5e887e72397da90a89d35e66bf
Bayekula-hb <hobedbayekula@gmail.com>
2021-08-11 16:25:33 +0100

    feat finish the first section of the header

--- components/header.jsx
+++ components/header.jsx
@@ -1,4 +1,8 @@
 import { useState, useEffect } from "react";
+import Image from 'next/image';
+import fb from "../public/images/facebook-brands.svg";
+import instagram from "../public/images/instagram-brands.svg";
+import twitter from "../public/images/twitter-square-brands.svg";
 
 const Header =()=>{
     const [dateState, setdateState] = useState("");
@@ -14,7 +18,13 @@ const Header =()=>{
             <div className="div-container container">
                 <div className="div-date-reseaux row">
                     <div className="div-date col-3">{dateState}</div>
-                    <div className="div-reseaux col-4">face instagr </div>
+                    <div className="div-reseaux col-4">
+                        <ul>
+                            <li className="link-reseaux"><a href="facebook.com"><Image src={fb} alt="facebook"/></a></li>
+                            <li className="link-reseaux"><a href="facebook.com"><Image src={instagram} alt="facebook"/></a></li>
+                            <li className="link-reseaux"><a href="facebook.com"><Image src={twitter} alt="facebook"/></a></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </header>
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,3 @@
-import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../sass/main.scss"
 
